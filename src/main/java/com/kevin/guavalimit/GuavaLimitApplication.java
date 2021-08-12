b5f749037e84bf1dd6d18eaa5e7a887d4ed879e4
chenligeng <2447925377@qq.com>
2021-08-12 10:26:54 +0800

    测试

---
+++ src/main/java/com/kevin/guavalimit/GuavaLimitApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.guavalimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaLimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavaLimitApplication.class, args);
+    }
+
+}
