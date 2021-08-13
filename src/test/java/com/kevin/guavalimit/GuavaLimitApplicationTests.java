9519a92eed8ce4638d2a809b72aefcc2d223b991
chenligeng <2447925377@qq.com>
2021-08-13 10:39:20 +0800

    sentinel

--- src/test/java/com/kevin/guavalimit/GuavaLimitApplicationTests.java
+++ src/test/java/com/kevin/guavalimit/GuavaLimitApplicationTests.java
@@ -1,13 +1,13 @@
 package com.kevin.guavalimit;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GuavaLimitApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GuavaLimitApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
